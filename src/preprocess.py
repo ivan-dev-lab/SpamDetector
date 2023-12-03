85c4e531b62e8fb07dbd6ab5c1fb8979c5bb19af
ivan-dev-lab <roalivan4@gmail.com>
2023-12-03 22:51:14 +0500

    исправлены недочеты

--- src/preprocess.py
+++ src/preprocess.py
@@ -16,12 +16,12 @@ def clean_text (data: pd.DataFrame) -> pd.DataFrame:
     sw = stopwords.words('russian')
     i = 1
 
-    for doc in data['text']:
+    for doc in data:
         cleaned_doc = nltk.word_tokenize(re.sub(r'\s+', ' ', re.sub(r'\W',' ',doc)).lower())
         cleaned_doc = [word for word in cleaned_doc if word not in sw]
         cleaned_text.append(' '.join(cleaned_doc))
 
-        logging.log(level=logging.INFO, msg=f'очищен текст {i}/{len(data["text"])} || {round((i*100)/len(data["text"]), 2)}%')
+        logging.log(level=logging.INFO, msg=f'очищен текст {i}/{len(data)} || {round((i*100)/len(data), 2)}%')
         i+=1
     return cleaned_text
 
@@ -29,7 +29,7 @@ def lemmatize_data (data: pd.DataFrame) -> pd.DataFrame:
     lemmatized_text = []
     morph_analyzer = pymorphy2.MorphAnalyzer(lang='ru')
     i = 1
-    for doc in data['text']:
+    for doc in data:
         tokenized_doc = nltk.word_tokenize(doc)
         lemmatized_words = []
         for word in tokenized_doc:
@@ -40,7 +40,7 @@ def lemmatize_data (data: pd.DataFrame) -> pd.DataFrame:
                 lemmatized_words.append(lemmatized_word.normal_form)
 
         lemmatized_text.append(' '.join(lemmatized_words))
-        logging.log(level=logging.INFO, msg=f'лемматизирован текст {i}/{len(data["text"])} || {round((i*100)/len(data["text"]),2)}%')
+        logging.log(level=logging.INFO, msg=f'лемматизирован текст {i}/{len(data)} || {round((i*100)/len(data),2)}%')
         i+=1
 
     
